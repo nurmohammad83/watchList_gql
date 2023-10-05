3399c9be31c1dbce363bd8c94d17d1b8743aa7f2
Nur Mohammad <mohammadnur5480@gmail.com>
2023-10-05 19:34:54 +0600

    feat: setup redux store

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/routes.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
 const client = new ApolloClient({
   uri: import.meta.env.VITE_GRAPHQL_URL,
@@ -14,7 +16,9 @@ const client = new ApolloClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <RouterProvider router={routes} />
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+      </Provider>
     </ApolloProvider>
   </React.StrictMode>
 );
